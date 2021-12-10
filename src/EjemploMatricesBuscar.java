9959810693eabbd8a19e8ab29b8a171c2ee19d73
jocheda <jocheda2@alumno.uned.es>
2021-12-10 08:21:16 +0100

    EjemploMatricesbuscar

--- src/EjemploMatricesBuscar.java
+++ src/EjemploMatricesBuscar.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class EjemploMatricesBuscar {
+public class EjemploMatricesBuscar {
+    public static void main(String[] args) {
+        int[][] matrizDeEnteros = {
+                {35, 90, 3, 1978},
+                {15, 2020, 10, 5},
+                {677, 127, 23767, 1999}
+
+        };
+
+        int elementoBuscar = 1999;
+        boolean encontrado = false;
+        int i = 0;
+        int j = 0;
+
+        buscar:
+        for (; i < matrizDeEnteros.length; i++) {
+            for (j = 0; j < matrizDeEnteros[i].length; j++) {
+                if (matrizDeEnteros[i][j] == elementoBuscar) {
+                    encontrado = true;
+                    break buscar;
+                }
+            }
+        }
+
+        if (encontrado) {
+            System.out.println("Encontrado " + elementoBuscar + " en las coordenadas " + i + "," + j);
+        } else {
+            System.out.println(elementoBuscar + " no se encontró en la matriz");
+        }
+    }
 }
